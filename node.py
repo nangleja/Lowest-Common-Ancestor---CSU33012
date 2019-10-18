69831188fdb86b75aa7a664263b1718803801d5d
James Nangle <nangleja@tcd.ie>
2019-10-18 15:11:11 +0100

    New Approach

--- node.py
+++ node.py
@@ -2,62 +2,18 @@
 class Node:
     def __init__(self, val):
         self.val =  val
-        self.left = None
-        self.right = None
-        self.travelled = False;
+        self.child = []
+        self.parent = []
+        self.depthMax = 0;
 
-def findPath( root, path, k):
+def addChild(self, n):
+    self.child.append(n)
+    n.parent.extend(self.parent)
+    n.parent.append(self)
+    if n.depthMax < self.depthMax + 1:
+        n.depthMax = self.depthMax + 1:
 
-    if root is None:
-        return False
-
-    path.append(root.val)
-
-    if root.val == k :
-        return True
-
-    if ((root.left != None and findPath(root.left, path, k)) or
-            (root.right!= None and findPath(root.right, path, k))):
-        return True
-
-
-    path.pop()
-    return False
-
-def LCA(root, x, y):
-    if(root is not None):
-        if(root.left is None and root.right is None and (x is not y)):
-            return -1
-    return LCARecursive(root, x, y, [False], [False])
-
-def LCARecursive(root, x, y, path1, path2):
-    if(root is None):
-        return -1
-
-    if(root.travelled is True):
-        return -1
-
-    left = LCARecursive(root.left, x, y, path1, path2)
-    right = LCARecursive(root.right, x, y, path1, path2)
-
-    if(root.val is x):
-        path1[0]=True
-        return root.val
-
-    if(root.val is y):
-        path2[0]=True            
-        return root.val
-
-    if(path1[0] is True and path2[0] is True):
-        if(left is not -1 and right is not -1):
-            return root.val
-        elif(left is not -1):
-            return left
-        else:
-            return right
-    if(left is not -1):
-        return left
-    if(right is not -1):
-        return right
-
-    return -1
+def printchild(self):
+    print("Parents of ", self.val)
+    for x in self.parents:
+        print("Parent = ", x.val, ", Depth = ", x.depthMax, "\n")
